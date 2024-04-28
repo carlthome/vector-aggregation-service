5776fe55dc871efcd6a36d861c61644d8c76ec85
Carl Thomé <carlthome@gmail.com>
2024-04-28 13:53:52 +0200

    Use gonum

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
+	// Initialize with the length of the vector, followed by a slice of floats containing the data.
+	u := mat.NewVecDense(3, []float64{1, 2, 3})
+	v := mat.NewVecDense(3, []float64{4, 5, 6})
+
+	// Add the vectors u and v and save results in u.
+	u.AddVec(u, v)
+
+	// Display the sum of the vectors.
+	fmt.Printf("The sum of the vectors is: %v\n", u)
+
 	// Register a handler for the root path "/"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Check if the request method is GET
